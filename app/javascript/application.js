afeb2e1083f20cc4c342274b33a21970346b7025
Mina Eissa <minaeissa268@gmail.com>
2022-12-07 15:23:48 +0200

    begin from 0 again

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,17 +2,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-require("./components/lib/slickgrid/slick.core")
-require("./components/lib/slickgrid/slick.dataview")
-require("./components/lib/slickgrid/slick.grid")
-require("./components/lib/slickgrid/slick.pager")
-require("./components/lib/d3.parcoords")
-require("./components/lib/d3.svg.multibrush")
-require("./components/lib/divgrid")
-require("./components/lib/scatterplot")
-require("./components/lib/superformula")
-require("./components/lib/sylvester")
-require("./components/lib/underscore")
-require("./components/lib/underscore.math")
-require("./components/Excercise_analysis")
-import "./css/site.scss"
